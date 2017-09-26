310a1c7dbf29a8e90e15f2808d34db283bd5f165
KIHOON-PARK <arlecchino12@naver.com>
2017-09-26 17:19:31 +0900

    1. 인벤토리 sceen1 에 인벤토리 메뉴 패널로 연결 2. 스케치업-교실 맵 test version 추가(collider미구현) 3. 스탠다드 에셋 추가 > 1인칭 캐릭터 추가 (스탠다드 에셋 통으로 안넣으면 컴파일 에러 .  나중에 필요없는 파일 지우기)

---
+++ EscapeRoom_1/Assets/button_c.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_c : MonoBehaviour {
+
+	public GameObject Building;
+
+	public void BuildingSetActive()
+	{
+		Building.SetActive (!Building.active);
+	}
+}
