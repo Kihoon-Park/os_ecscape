4e0ad5f24782527de6fa8a486a9fcd96d8660688
KIHOON-PARK <arlecchino12@naver.com>
2017-09-28 16:27:19 +0900

    map 업데이트/사이즈 0.1 > 0.05로 조정 game manager 생성 > Tab키로 game mode/uimode 변경 scripts 폴더 추가 > script는 폴더 안에

---
+++ EscapeRoom_1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public enum GameMode
+{
+    FPS_MODE,
+    UI_MODE,
+};
+
+public class GameManager : MonoBehaviour
+{
+    GameMode gameMode = GameMode.FPS_MODE;
+    public GameObject uiCanvas;
+    public FirstPersonController fpsController;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            switch (gameMode)
+            {
+                case GameMode.FPS_MODE:
+                    ChangeMode(GameMode.UI_MODE);
+                    break;
+                case GameMode.UI_MODE:
+                    ChangeMode(GameMode.FPS_MODE);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+
+    void ChangeMode(GameMode mode)
+    {
+        switch (mode)
+        {
+            case GameMode.FPS_MODE:
+                uiCanvas.SetActive(false);
+                fpsController.enabled = true;
+                break;
+            case GameMode.UI_MODE:
+                uiCanvas.SetActive(true);
+                fpsController.enabled = false;
+                break;
+            default:
+                break;
+        }
+
+        gameMode = mode;
+    }
+}
