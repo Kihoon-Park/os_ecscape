5cb5e38f691af2b9ee48171a838c8e1f3590c04f
PKH\arlec <gh9411@gmail.com>
2017-11-07 18:13:21 +0900

    문짝 뜯어고침 //door1 완성/ door2삭제 //rigidbody까지 완료

---
+++ EscapeRoom_1/Assets/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    bool bOpen = false;
+    public float speed;
+    public GameObject door;
+    // Use this for initialization
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform == transform)
+                {
+                    float end = bOpen ? 90 : 0;
+                    Vector3 rot = new Vector3(-90, 0, -end);
+                    iTween.RotateTo(door, rot, speed);
+
+                    bOpen = !bOpen;
+                }
+            }
+        }
+    }
+}
