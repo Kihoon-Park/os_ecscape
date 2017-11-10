5acde20a0bc1ded20974fc678c818aa2640b07f7
Unknown <jsstar1207@naver.com>
2017-11-10 15:19:30 +0900

    여러개의 cube를 잡는 코드 추가

--- EscapeRoom_1/Assets/Scripts/OnHandTest.cs
+++ EscapeRoom_1/Assets/Scripts/OnHandTest.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OnHandTest : MonoBehaviour {
-   public Transform obj;
-   bool onHand = false;
-   // Use this for initialization
-   void Start () {
-      
-   }
-   
-   // Update is called once per frame
-   void Update () {
-      if(Input.GetMouseButton(0))
-      {
-         if(onHand)
-         {
-            obj.transform.parent = null;
-         }
-         else
-         {
-            obj.transform.parent = transform;
-            obj.transform.position = transform.position + transform.forward * 6f;
-         }
+    public Transform obj1;
+    public Transform obj2;
+    bool onHand = false;
 
-         onHand = !onHand;
-      }
-   }
+    void Start () 
+    {
+
+    }
+
+    void Update ()
+    {
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            if(Vector3.Distance(transform.position, obj1.transform.position) <= 5)
+            {
+                if(onHand)
+                {
+                    obj1.transform.parent = null;
+                }
+                else
+                {
+                    obj1.transform.parent = transform;
+                    obj1.transform.position = transform.position + transform.forward * 5f;
+                }
+
+                onHand = !onHand;
+            }
+            else if(Vector3.Distance(transform.position, obj2.transform.position) <= 5)
+            {
+                if(onHand)
+                {
+                    obj2.transform.parent = null;
+                }
+                else
+                {
+                    obj2.transform.parent = transform;
+                    obj2.transform.position = transform.position + transform.forward * 5f;
+                }
+
+                onHand = !onHand;
+            }
+        }
+    }
 }
