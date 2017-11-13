443df43d6349c1afa1cd4687c3e73ca26ebee676
JANGDUCKIN <77999jno@naver.com>
2017-11-13 15:25:17 +0900

    손전등 추가 - 위치변환 수정 예정

--- EscapeRoom_1/Assets/Scripts/OnHandTest.cs
+++ EscapeRoom_1/Assets/Scripts/OnHandTest.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OnHandTest : MonoBehaviour {
     public Transform obj1;
     public Transform obj2;
+	public Transform torc;
     bool onHand = false;
 
     void Start () 
@@ -45,6 +46,20 @@ public class OnHandTest : MonoBehaviour {
 
                 onHand = !onHand;
             }
+			else if(Vector3.Distance(transform.position, torc.transform.position) <= 5)
+			{
+				if(onHand)
+				{
+					torc.transform.parent = null;
+				}
+				else
+				{
+					torc.transform.parent = transform;
+					torc.transform.position = transform.position + transform.forward * 5f;
+				}
+
+				onHand = !onHand;
+			}
         }
     }
 }
