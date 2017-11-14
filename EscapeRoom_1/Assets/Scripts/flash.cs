08cc80cf672bd4c165ca938a3f1536526dd4c3ee
JANGDUCKIN <77999jno@naver.com>
2017-11-14 16:21:55 +0900

    손전등 조명 F누르면 켜지게 설

---
+++ EscapeRoom_1/Assets/Scripts/flash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flash : MonoBehaviour {
+
+	public Light flight;
+	public bool on = true;
+	// Use this for initialization
+	void Start () {
+		on = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if (on == true) {
+				flight.enabled = false;
+				on = false;
+			} else if (on == false) {
+				flight.enabled = true;
+				on = true;
+			}
+		}
+	}
+}
