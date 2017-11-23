ce45c9f4a1dec063b2223edf3207f5fa4dcac5e6
PKH\arlec <gh9411@gmail.com>
2017-11-23 19:11:01 +0900

    password.cs Doorword.cs로 합병(password.cs 삭제) button_c.cs 삭제 나가기 버튼 따로 만듬

--- EscapeRoom_1/Assets/Scripts/doorlock.cs
+++ EscapeRoom_1/Assets/Scripts/doorlock.cs
@@ -1,17 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class doorlock : MonoBehaviour {
+public class Doorlock : MonoBehaviour
+{
     public GameObject passwordPanel;
-
+    public Text passwordText;
 
     // Use this for initialization
-    void Start () 
+    void Start()
     {
+        passwordText.text = "";
+    }
 
-	}
+    public void Inputnum(int num)
+    {
+        if (num == 1)
+            passwordText.text += 1;
+        if (num == 2)
+            passwordText.text += 2;
+        if (num == 3)
+            passwordText.text += 3;
+        if (num == 4)
+            passwordText.text += 4;
+        if (num == 5)
+            passwordText.text += 5;
+        if (num == 6)
+            passwordText.text += 6;
+        if (num == 7)
+            passwordText.text += 7;
+        if (num == 8)
+            passwordText.text += 8;
+        if (num == 9)
+            passwordText.text += 9;
+        if (num == 0)
+            passwordText.text += 0;
+    }
 
+    public void Clearnum()
+    {
+        passwordText.text = "";
+    }
+
+    public void Gameclear()
+    {
+        if (passwordText.text == "4321")
+            SceneManager.LoadScene(4);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -30,5 +67,5 @@ public class doorlock : MonoBehaviour {
             }
         }
     }
-    
+
 }
